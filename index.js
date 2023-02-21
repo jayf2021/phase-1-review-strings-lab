9ee9efda64f1f22d88d4bc9457d9f134e5487c09
James Fields <fieldsjay13@gmail.com>
2023-02-21 11:09:16 -0700

    complete lab

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
-
-
-
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
 
 
